5933f9bc0cdb1a7d1bdc89247148a5b9342a55bc
Gijela <961323961@qq.com>
2024-11-25 00:41:56 +0800

    feat(landing-page): 添加仪表盘页面和相关组件
    - 新增 Dashboard 组件作为仪表盘页面的主组件
    - 添加 Sidebar、Header 和 Banner 组件用于仪表盘的各个部分
    - 实现了基本的仪表盘布局和样式
    - 包含了下载 GitHub 模板和检查高级版本的 Banner
    - Header 组件中集成了搜索按钮和一些快捷操作

--- packages/landing-page/tailwind.config.js
+++ packages/landing-page/tailwind.config.js
@@ -1,8 +1,26 @@
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
-	plugins: [require('daisyui')],
-	daisyui: {
-		themes: ['corporate', 'business'],
-	},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  daisyui: {
+    themes: ["corporate", "business"],
+  },
+  plugins: [
+    require("daisyui"),
+    plugin(({ addVariant, e }) => {
+      addVariant("sidebar-expanded", ({ modifySelectors, separator }) => {
+        modifySelectors(
+          ({ className }) =>
+            `.sidebar-expanded .${e(
+              `sidebar-expanded${separator}${className}`
+            )}`
+        );
+      });
+    }),
+  ],
 };
