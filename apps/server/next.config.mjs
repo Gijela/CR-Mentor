b20daa032deffef5a9eacd8137456b5945dbbcf2
Gijela <961323961@qq.com>
2024-12-18 01:45:56 +0800

    feat: core

--- apps/server/next.config.mjs
+++ apps/server/next.config.mjs
@@ -1,13 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'cr-mentor.top',
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cr-mentor.top",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // 匹配所有 API 路由
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*.cr-mentor.top",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
