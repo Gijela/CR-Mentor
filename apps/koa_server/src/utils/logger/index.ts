f802b72a1d0d270e0d00b49f1f5574c969c2013e
Gijela <linjianhui1@xdf.xn>
2025-03-05 19:37:17 +0800

    feat(utils): add module dependency grouping utility
    - Implement `groupModulesByDependency` function to analyze dependency relationships
    - Create utility for identifying related modules based on dependency graph
    - Add comprehensive logging and testing mechanism for module dependency analysis
    - Update logger to support more flexible logging of different data types

--- apps/koa_server/src/utils/logger/index.ts
+++ apps/koa_server/src/utils/logger/index.ts
@@ -1,10 +1,10 @@
 // 日志工具
 const logger = {
-  info: (message: string, ...args: any[]) => {
+  info: (message: any, ...args: any[]) => {
     // eslint-disable-next-line no-console
     console.log(message, ...args)
   },
-  error: (message: string, ...args: any[]) => {
+  error: (message: any, ...args: any[]) => {
     // eslint-disable-next-line no-console
     console.error(message, ...args)
   },
